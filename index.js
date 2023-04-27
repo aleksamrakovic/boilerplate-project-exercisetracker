1b92fb928440050765636264411bd888131e16cd
Aleksa Mrakovic <45458632+aleksamrakovic@users.noreply.github.com>
2023-04-27 17:46:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -91,19 +91,27 @@ app.post("/api/users", (req, res) => {
       });
     }
 
-    db.run(
-      "INSERT INTO users(username) VALUES (?)",
-      [username],
-      function (err) {
-        if (err) {
-          return res.status(500).json({
-            status: 500,
-            success: false,
-            error: "Internal Server Error",
-          });
-        }
-
-        db.get(`SELECT * FROM users WHERE id = ?`, this.lastID, (err, row) => {
+    db.get("SELECT * FROM users WHERE username = ?", username, (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return res.status(400).json({
+          status: 500,
+          success: false,
+          error: "Something went wrong",
+        });
+      }
+      if (row) {
+        return res.status(400).json({
+          status: 400,
+          success: false,
+          error: "User already exists",
+        });
+      }
+      // If username is unique and non-empty, insert it into the database
+      db.run(
+        "INSERT INTO users(username) VALUES (?)",
+        [username],
+        function (err) {
           if (err) {
             return res.status(500).json({
               status: 500,
@@ -112,14 +120,28 @@ app.post("/api/users", (req, res) => {
             });
           }
 
-          return res.status(200).json({
-            status: 200,
-            success: true,
-            data: row,
-          });
-        });
-      }
-    );
+          db.get(
+            `SELECT * FROM users WHERE id = ?`,
+            this.lastID,
+            (err, row) => {
+              if (err) {
+                return res.status(500).json({
+                  status: 500,
+                  success: false,
+                  error: "Internal Server Error",
+                });
+              }
+
+              return res.status(200).json({
+                status: 200,
+                success: true,
+                data: row,
+              });
+            }
+          );
+        }
+      );
+    });
   } catch (error) {
     return res.status(400).json({
       status: 400,
@@ -134,60 +156,106 @@ app.post("/api/users/:id/exercises", (req, res) => {
     const userId = parseInt(req.params.id);
     const { description, duration, date } = req.body;
 
-    // Validate request body
-    if (!description || !duration) {
+    // Validate user id
+    if (!userId) {
       return res.status(400).json({
         status: 400,
         success: false,
-        error: "Description and duration are required fields",
+        error: "User id is required param",
       });
     }
 
-    // Default to current date if not provided
-    const exerciseDate = date ? new Date(date) : new Date();
+    // Check if user exists
+    db.get(`SELECT * FROM users WHERE id = ?`, [userId], (err, row) => {
+      if (err) {
+        return res.status(500).json({
+          status: 500,
+          success: false,
+          error: "Internal Server Error",
+        });
+      }
+      if (!row) {
+        return res.status(400).json({
+          status: 400,
+          success: false,
+          error: "User does not exist",
+        });
+      }
 
-    db.run(
-      "INSERT INTO exercises(userId, description, duration, date) VALUES (?, ?, ?, ?)",
-      [userId, description, duration, exerciseDate.toISOString()],
-      function (err) {
-        if (err) {
-          return res.status(500).json({
-            status: 500,
+      // Validate request body
+      if (!description || !duration) {
+        return res.status(400).json({
+          status: 400,
+          success: false,
+          error: "Description and duration are required fields",
+        });
+      }
+
+      // Check if duration is negative
+      if (duration < 0) {
+        return res.status(400).json({
+          status: 400,
+          success: false,
+          error: "Duration cannot be negative",
+        });
+      }
+
+      // Check if date is provided and in the correct format (yyyy-mm-dd)
+      if (date) {
+        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+        if (!dateRegex.test(date)) {
+          return res.status(400).json({
+            status: 400,
             success: false,
-            error: "Internal Server Error",
+            error: "Invalid date format. Date should be in yyyy-mm-dd format",
           });
         }
+      }
 
-        // Get the newly inserted exercise record
-        db.get(
-          `SELECT * FROM exercises WHERE id = ?`,
-          this.lastID,
-          (err, row) => {
-            if (err) {
-              return res.status(500).json({
-                status: 500,
-                success: false,
-                error: "Internal Server Error",
-              });
-            }
+      // Default to current date if not provided
+      const exerciseDate = date ? new Date(date) : new Date();
 
-            console.log(row);
-
-            return res.status(200).json({
-              status: 200,
-              success: true,
-              data: {
-                exerciseId: row.ID,
-                userId: row.userId,
-                description: row.description,
-                duration: row.duration,
-                date: row.date,
-              },
+      db.run(
+        "INSERT INTO exercises(userId, description, duration, date) VALUES (?, ?, ?, ?)",
+        [userId, description, duration, exerciseDate.toISOString()],
+        function (err) {
+          if (err) {
+            return res.status(500).json({
+              status: 500,
+              success: false,
+              error: "Internal Server Error",
             });
           }
-        );
-      }
-    );
+
+          // Get the newly inserted exercise record
+          db.get(
+            `SELECT * FROM exercises WHERE id = ?`,
+            this.lastID,
+            (err, row) => {
+              if (err) {
+                return res.status(500).json({
+                  status: 500,
+                  success: false,
+                  error: "Internal Server Error",
+                });
+              }
+
+              return res.status(200).json({
+                status: 200,
+                success: true,
+                data: {
+                  exerciseId: row.ID,
+                  userId: row.userId,
+                  description: row.description,
+                  duration: row.duration,
+                  date: row.date,
+                },
+              });
+            }
+          );
+        }
+      );
+    });
   } catch (error) {
     return res.status(400).json({
       status: 400,
